11a9af14b485b09ff814d384c4909b619ef4e331
Maximilian Stephan <stephan.maxi@icloud.com>
2025-04-01 22:27:15 +0200

    feat(drivers): add drivers
    The first driver to be added is spi_sdcard.
    This driver handles I/O with SPI uSD breakout boards and
    implements functions for writing, reading, erasing and
    querying the uSD flash.

---
+++ src/include/aurora/drivers/sdcard_io.h
@@ -0,0 +1,14 @@
+/**
+* @file sdcard_io.h
+* @brief I/O library for SDCards
+* @note This file contains the function prototypes and definitions for
+* sdcard I/O.
+*
+* Author: Maximilian Stephan @ Auxspace e.V.
+* Copyright (C) 2025 Auxspace e.V.
+*/
+
+#ifndef SDCARD_IO_H
+#define SDCARD_IO_H
+
+#endif /* SDCARD_IO_H */
---
+++ src/include/aurora/drivers/spi_flash.h
@@ -0,0 +1,14 @@
+/**
+* @file spi_flash.h
+* @brief SPI Flash library
+* @note This file contains the function prototypes and definitions for
+* SPI flashes.
+*
+* Author: Maximilian Stephan @ Auxspace e.V.
+* Copyright (C) 2025 Auxspace e.V.
+*/
+
+#ifndef SPI_FLASH_H
+#define SPI_FLASH_H
+
+#endif /* SPI_FLASH_H */
